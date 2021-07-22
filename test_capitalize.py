b296da6ef3c568ffb25f08aca2edce547660c0d6
Vinay Kumar Ranganath <vinaykumar.ranganath@softwareag.com>
2021-07-22 19:03:41 +0530

    second commit

---
+++ test_capitalize.py
@@ -0,0 +1,8 @@
+def capital_case(x):
+    return x.capitalize()
+
+def test_capital_case():
+    assert capital_case('semaphore') == 'Semaphore'
+
+def test_capital_case1():
+    print("Tests passed")
